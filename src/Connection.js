e551f74dee9a5817d02e7eafc42f7a14e955d52a
Rafael Pereira de Andrade <rafaelpda7@gmail.com>
2023-04-16 01:56:51 -0300

    Projeto iniciado - 1a Sprint

---
+++ src/Connection.js
@@ -0,0 +1,17 @@
+const knex = require('knex')({
+
+    client: 'pg',
+
+    connection: {
+        host: 'localhost',
+        port: 5000,
+        user: 'postgres',
+        password: 'tretAdo66',
+        database: 'pdv'
+    },
+
+});
+
+
+
+module.exports = knex;
